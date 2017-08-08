3db0342143fe171a632e86f90ead7967fcea9402
Kathryn Killebrew <kkillebrew@azavea.com>
2017-08-08 13:51:37 -0400

    Create model for indicator parameter
    Specify type for indicator parameter description, instead of using any.

---
+++ src/app/models/indicator-parameter.model.ts
@@ -0,0 +1,8 @@
+/* tslint:disable:variable-name */
+export class IndicatorParameter {
+    name: string;
+    description: string;
+    required: boolean;
+    default?: any;
+}
+/* tslint:enable:variable-name */
